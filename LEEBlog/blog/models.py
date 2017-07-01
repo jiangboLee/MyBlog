f50797e687689d9597c4e0eac9b227b4f4560faa
李江波 <584556825@qq.com>
2017-07-01 12:44:10 +0800

    首页OK

--- LEEBlog/blog/models.py
+++ LEEBlog/blog/models.py
@@ -5,9 +5,13 @@ from django.contrib.auth.models import User
 #分类
 class Category(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 #标签
 class Tag(models.Model):
     name = models.CharField(max_length=100)
+    def __str__(self):
+        return self.name
 
 class Post(models.Model):
     title = models.CharField(max_length=70)
@@ -16,3 +20,7 @@ class Post(models.Model):
     modified_time = models.DateTimeField() #修改时间
     excerpt = models.CharField(max_length=200, blank=True)
     category = models.ForeignKey(Category) #一对多
+    tags = models.ManyToManyField(Tag, blank=True) #多对多
+    author = models.ForeignKey(User)
+    def __str__(self):
+        return self.title
